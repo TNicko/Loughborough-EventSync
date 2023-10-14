9637d590fa2e05d57e6d2be3f4ec03e267c4f939
Nickolai Tchesnokov <nickolaitchesnokov@eduroam-student-pt11-78-230.lut.ac.uk>
2023-10-14 17:52:41 +0100

    polyfill cross platform web-extention package installed. handleSubmit for download button created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import './App.css'
+import browser from "webextension-polyfill"
 
 function App() {
+  
+  const handleSubmit = async () => {
+    try {
+      const tabs = await browser.tabs.query({ active: true, currentWindow: true})
+      const currentTab = tabs[0];
+
+      if (currentTab && currentTab.url?.startsWith('https://lucas.lboro.ac.uk/its_apx/f?p=250')) {
+        console.log("already on url");
+      } else {
+        await browser.tabs.create({url: 'https://lucas.lboro.ac.uk/its_apx/f?p=250'})
+        console.log("opened calender url");
+      }
+    } catch (error) {
+      console.error("Error accessing tabs: ", error);
+    } 
+  }
+
   return (
     <>
       <div className="container">
@@ -10,7 +28,7 @@ function App() {
         <div className="subtext">
           Export or Sync your events
         </div>
-        <button className="download-btn">Download Calender iCal</button>
+        <button className="download-btn" onClick={handleSubmit}>Download Calender iCal</button>
       </div>
     </>
   )
