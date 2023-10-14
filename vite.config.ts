3996e8873eea2cbecd53782436bd76aa00a6cda2
czuhajster <55452766+czuhajster@users.noreply.github.com>
2023-10-14 18:16:26 +0100

    Add alias for the src dir to the Vite config file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
+
+const resolveDir = dir => path.resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        '@': resolveDir('./src'),
+    }
+  }
 })
