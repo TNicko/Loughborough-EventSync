6cd79ef2ebf6da948b78b499b3347d78042864a7
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2023-12-02 20:58:59 +0000

    deleted content and background scripts

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,45 +1,52 @@
 import '../global.css'
-import browser from "webextension-polyfill"
+import browser from 'webextension-polyfill'
 
 function App() {
-  
   const handleSubmit = async () => {
     try {
-      const tabs = await browser.tabs.query({ active: true, currentWindow: true})
+      const tabs = await browser.tabs.query({
+        active: true,
+        currentWindow: true,
+      })
       const activeTab = tabs[0]
       console.log(activeTab.url)
-      if (activeTab.url && activeTab.url.startsWith('https://lucas.lboro.ac.uk/its_apx/f?p=250')) {
-        console.log("Scraping tab: ", activeTab.id);
-        
+      if (
+        activeTab.url &&
+        activeTab.url.startsWith('https://lucas.lboro.ac.uk/its_apx/f?p=250')
+      ) {
+        console.log('Scraping tab: ', activeTab.id)
+
         // Sending the message and waiting for a response
         if (activeTab.id !== undefined) {
-          browser.tabs.sendMessage(activeTab.id, { command: "scrapePage" })
-            .then(response => {
-              console.log("Received response:", response);
+          browser.tabs
+            .sendMessage(activeTab.id, { action: 'runContentScript' })
+            .then((response) => {
+              console.log('Received response:', response)
             })
-            .catch(reportError);
+            .catch(reportError)
         } else {
-          console.log("Tab is not fully initialised: ID undefined.")
+          console.log('Tab is not fully initialised: ID undefined.')
         }
-
       } else {
-        console.log("Not currently on correct url!");
+        console.log('Not currently on correct url!')
       }
     } catch (error) {
-      console.error("Error accessing tabs: ", error);
-    } 
+      console.error('Error accessing tabs: ', error)
+    }
   }
 
   return (
     <>
-      <div className="container">
-        <div className="header">
-          Loughborough<br/>Event Sync
+      <div className='container'>
+        <div className='header'>
+          Loughborough
+          <br />
+          Event Sync
         </div>
-        <div className="subtext">
-          Export or Sync your events
-        </div>
-        <button className="download-btn" onClick={handleSubmit}>Download Calender iCal</button>
+        <div className='subtext'>Export or Sync your events</div>
+        <button className='download-btn' onClick={handleSubmit}>
+          Download Calender iCal
+        </button>
       </div>
     </>
   )
